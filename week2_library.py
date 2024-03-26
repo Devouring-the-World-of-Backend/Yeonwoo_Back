1fb38771128e25adae427b500af007fedde669ae
ywnjeong <wjddusdn1237@gmail.com>
2024-03-27 01:25:35 +0900

    feat: add OPTIONS to CORS

--- week2_library.py
+++ week2_library.py
@@ -17,7 +17,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],      #OPTIONS 추가
     allow_headers=["*"],
 )
 
